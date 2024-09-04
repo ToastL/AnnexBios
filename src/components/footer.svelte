ca8159406b15c42020e52611d708bbb06ab61a17
LucasDG1 <144344269+LucasDG1@users.noreply.github.com>
2024-09-04 13:38:21 +0200

    footer

---
+++ src/components/footer.svelte
@@ -0,0 +1,14 @@
+<footer class="w-full h-[430px] bg-[#666666]">
+   <div class="flex justify-between items-center border border-white w-9/12 h-[430px] pl-28 pr-64">
+        <div class="border border-white h-64">
+            <div class="background-image: url('name.png')"></div>
+        </div>
+        <div class="border border-white h-[225px]">ya yeet</div>
+        <div class="border border-white h-64">ya yeet</div>
+   </div> 
+   <div class=""></div>
+
+</footer>
+<div class="w-full h-11 bg-black">
+    
+</div>
