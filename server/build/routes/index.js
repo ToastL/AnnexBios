9b79abd6ea0b9678d6437e2e14ffd7e8e9044b29
Toaster5 <etiendenouden2006@gmail.com>
2024-09-05 16:08:04 +0200

    server

---
+++ server/build/routes/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.movies = exports.hello = void 0;
+var _hello = _interopRequireWildcard(require("./ping"));
+exports.hello = _hello;
+var _movies = _interopRequireWildcard(require("./movies"));
+exports.movies = _movies;
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function (e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sInNvdXJjZXMiOlsiLi4vLi4vc3JjL3JvdXRlcy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBhcyBoZWxsbyBmcm9tIFwiLi9waW5nXCJcblxuZXhwb3J0ICogYXMgbW92aWVzIGZyb20gXCIuL21vdmllc1wiIl0sIm1hcHBpbmdzIjoiIiwiaWdub3JlTGlzdCI6W119
