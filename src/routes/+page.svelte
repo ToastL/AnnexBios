11a0814aa2c072bba0c35ae628139800be236725
LucasDG1 <144344269+LucasDG1@users.noreply.github.com>
2024-09-05 11:33:37 +0200

    home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,18 @@
             <p class="mb-14 max-w-[50rem] text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea aspernatur, placeat expedita quis repellat mollitia veritatis </p>
             <a class="uppercase font-semibold bg-white text-black p-3" href="">bekijk onze vestigingen</a>
         </div>
-        <div class="grid grid-cols-1 lg:grid-cols-2 w-full gap-10">
-            <div class="h-[18rem] w-full bg-black"></div>
-            <div class="h-[18rem] w-full bg-black"></div>
-            <div class="h-[18rem] w-full bg-black"></div>
-            <div class="h-[18rem] w-full bg-black"></div>
-            <div class="h-[18rem] w-full bg-black"></div>
-            <div class="h-[18rem] w-full bg-black"></div>
+        <div class="grid grid-cols-1 lg:grid-cols-3 w-full gap-9">
+            <div class="h-[22rem] w-full bg-white">
+                <img src="/src/assets/place.png" alt="">
+                <div class="text-3xl pl-7 pt-7 text-primary">HELLEVOETSLUIS</div>
+                <div class="text-xl text-primary pl-7 pt-2">Rijksstraatweg 42, 3223 KA</div>
+                <div class="bg-black w-[5rem] h-[4rem]"></div>
+            </div>
+            <div class="h-[22rem] w-full bg-black"></div>
+            <div class="h-[22rem] w-full bg-black"></div>
+            <div class="h-[22rem] w-full bg-black"></div>
+            <div class="h-[22rem] w-full bg-black"></div>
+            <div class="h-[22rem] w-full bg-black"></div>
         </div>
     </section>
 </main>
